6b2b0a36735a8080d3c2e7ed1fb6a59c5b359887
krall <luke.kral@seznam.cz>
2020-11-30 13:31:38 +0100

    update

--- src/Pagination/Pagination.php
+++ src/Pagination/Pagination.php
@@ -48,7 +48,7 @@ class Pagination extends Control
      */
     public function getLink(int $page): ?string
     {
-        if($page > 0 && is_callable($this->linkCallback))
+        if($page > 0 && $page <= $this->paginator->getPageCount() && is_callable($this->linkCallback))
         {
             $getLinkFn = $this->linkCallback;
             return $getLinkFn($page, $this->paginator);
