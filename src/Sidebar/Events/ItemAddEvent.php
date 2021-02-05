f047b8da58dd41016341a3a99beef4efdf4036e7
krall <luke.kral@seznam.cz>
2021-02-05 09:59:27 +0100

    contributte/event-dispatcher for Sidebar

--- src/Sidebar/Events/ItemAddEvent.php
+++ src/Sidebar/Events/ItemAddEvent.php
@@ -6,19 +6,4 @@ namespace e2221\BootstrapComponents\Sidebar\Events;
 
 class ItemAddEvent
 {
-    private string $itemId;
-
-    public function __construct(string $itemId)
-    {
-        $this->itemId = $itemId;
-    }
-
-    /**
-     * Get item id
-     * @return string
-     */
-    public function getItemId(): string
-    {
-        return $this->itemId;
-    }
 }
